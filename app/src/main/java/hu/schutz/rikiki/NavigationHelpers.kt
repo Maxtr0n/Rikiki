874ea4fe1d64a67d7dc0893a947ac72749bb1b01
Schütz Máté <schutz.mate@gmail.com>
2023-02-04 14:41:02 +0100

    feat(android): navigation work

--- app/src/main/java/hu/schutz/rikiki/NavigationHelpers.kt
+++ app/src/main/java/hu/schutz/rikiki/NavigationHelpers.kt
@@ -1,2 +1,42 @@
 package hu.schutz.rikiki
 
+import androidx.navigation.NavController
+import hu.schutz.rikiki.Screens.GAME_SCREEN
+import hu.schutz.rikiki.Screens.GAME_SETTINGS_SCREEN
+import hu.schutz.rikiki.Screens.MAIN_MENU_SCREEN
+
+private object Screens {
+    const val MAIN_MENU_SCREEN = "main_menu"
+    const val GAME_SCREEN = "game"
+    const val GAME_SETTINGS_SCREEN = "game_settings"
+}
+
+object RikikiNavigationArgs {
+
+}
+
+object RikikiDestinations {
+    const val MAIN_MENU_ROUTE = MAIN_MENU_SCREEN
+    const val GAME_ROUTE = GAME_SCREEN
+    const val GAME_SETTINGS_ROUTE = GAME_SETTINGS_SCREEN
+}
+
+class RikikiNavigationActions(private val navController: NavController) {
+    fun navigateToGameSettingsScreen() {
+        navController.navigate(GAME_SETTINGS_SCREEN) {
+            launchSingleTop = true
+        }
+    }
+
+    fun navigateToGameScreen() {
+        navController.navigate(GAME_SCREEN) {
+            launchSingleTop = true
+        }
+    }
+
+    fun navigateToMainMenuScreen() {
+        navController.navigate(MAIN_MENU_SCREEN) {
+            launchSingleTop = true
+        }
+    }
+}
--- app/src/main/java/hu/schutz/rikiki/RikikiNavHost.kt
+++ app/src/main/java/hu/schutz/rikiki/RikikiNavHost.kt
@@ -1,2 +1,25 @@
 package hu.schutz.rikiki
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import hu.schutz.rikiki.RikikiDestinations.MAIN_MENU_ROUTE
+
+@Composable
+fun RikikiNavHost(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier,
+    navigationActions: RikikiNavigationActions = remember(navHostController) {
+        RikikiNavigationActions(navHostController)
+    }
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = MAIN_MENU_ROUTE,
+        modifier = modifier
+    ) {
+
+    }
+}
